9ce6620ebfbdbeac231cca565ca186e88b8d72de
NhatNguyen1810 <nhat7203@gmail.com>
2020-07-16 20:49:51 -0700

    Updates

---
+++ precache-manifest.c2a1e4e233bc0e88524758f030077a93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be3c9e5252cf6558ab1223bb8fb14f1",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "68c9f23485eb44794677",
+    "url": "/todoapp/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "0ec63ce50bb138e8a5da",
+    "url": "/todoapp/static/css/main.c46b82b4.chunk.css"
+  },
+  {
+    "revision": "68c9f23485eb44794677",
+    "url": "/todoapp/static/js/2.b7689778.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todoapp/static/js/2.b7689778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec63ce50bb138e8a5da",
+    "url": "/todoapp/static/js/main.a87a59b5.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
